1f8036e73a2d913a867c34c2134ff2bdf5447ebf
Rajin Hossain <r.hossain@thisisaura.com>
2022-11-17 10:04:55 +1100

    more subtle UI customisations

--- web_view/web_view_test/lib/main.dart
+++ web_view/web_view_test/lib/main.dart
@@ -70,39 +70,40 @@ class _MyHomePageState extends State<MyHomePage> {
             // BELOW IS A CUSTOM WEB WIDGET //
             //                              //
             //////////////////////////////////
-            SizedBox(
-              child: Stack(children: [
-                AnimatedPositioned(
-                  duration: const Duration(seconds: 2),
-                  curve: Curves.fastOutSlowIn,
-                  child: Visibility(
-                    visible: _isVisible,
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(20),
-                              topRight: Radius.circular(20)),
-                          border: Border.all(
-                              color: const Color.fromRGBO(66, 165, 245, 1),
-                              style: BorderStyle.solid,
-                              width: 5,
-                              strokeAlign: StrokeAlign.outside)),
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            topRight: Radius.circular(15)),
-                        child: Column(
-                          children: [
-                            GestureDetector(
-                              onDoubleTap: toggleWebView,
-                              child: Container(
-                                  padding: const EdgeInsets.all(10),
-                                  color: const Color.fromRGBO(66, 165, 245, 1),
-                                  height: 60,
-                                  child: Row(
-                                    children: [
-                                      const SizedBox(
+            Visibility(
+              visible: _isVisible,
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(66, 165, 245, 1),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    border: Border.all(
+                        color: const Color.fromRGBO(66, 165, 245, 1),
+                        style: BorderStyle.solid,
+                        width: 5,
+                        strokeAlign: StrokeAlign.outside)),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onDoubleTap: toggleWebView,
+                        child: Container(
+                            padding: const EdgeInsets.all(10),
+                            color: const Color.fromRGBO(66, 165, 245, 1),
+                            height: 60,
+                            child: Row(
+                              children: [
+                                Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const [
+                                      SizedBox(
                                         child: Text(
                                           'Random Title',
                                           style: TextStyle(
@@ -111,60 +112,78 @@ class _MyHomePageState extends State<MyHomePage> {
                                               fontWeight: FontWeight.w900),
                                         ),
                                       ),
-                                      const Expanded(
-                                        child: SizedBox(
-                                          height: 40,
+                                      SizedBox(
+                                        child: Text(
+                                          '@Random_User',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.w500),
                                         ),
-                                      ),
-                                      DefaultButton(
-                                        text: 'Close',
-                                        onTap: toggleWebView,
-                                        color: Color.fromARGB(255, 0, 115, 160),
-                                      ),
-                                    ],
-                                  )),
-                            ),
-                            SingleChildScrollView(
-                              child: Container(
-                                height: 600,
-                                color: const Color.fromRGBO(66, 165, 245, 1),
-                                child: WebView(
-                                  initialUrl:
-                                      'https://twitter.com/Tesla/status/1582901412312207361',
-                                  javascriptMode: JavascriptMode.unrestricted,
-                                  onWebViewCreated: (controller) {
-                                    _controller.complete(controller);
-                                  },
-                                  onProgress: (int progress) {
-                                    print(
-                                        'WebView is loading (progress : $progress%)');
-                                  },
-                                  navigationDelegate:
-                                      (NavigationRequest request) {
-                                    if (request.url.startsWith(
-                                        'https://www.youtube.com/')) {
-                                      print('blocking navigation to $request}');
-                                      return NavigationDecision.prevent;
-                                    }
-                                    print('allowing navigation to $request');
-                                    return NavigationDecision.navigate;
-                                  },
-                                  onPageStarted: (String url) {
-                                    print('Page started loading: $url');
-                                  },
-                                  onPageFinished: (String url) {},
-                                  gestureNavigationEnabled: true,
-                                  backgroundColor: Colors.blue[400],
+                                      )
+                                    ]),
+                                const Expanded(
+                                  child: SizedBox(
+                                    height: 40,
+                                  ),
                                 ),
-                              ),
-                            )
-                          ],
-                        ),
+                                DefaultButton(
+                                  text: 'Close',
+                                  onTap: toggleWebView,
+                                  color:
+                                      const Color.fromARGB(255, 78, 137, 204),
+                                ),
+                              ],
+                            )),
                       ),
-                    ),
+                      Container(
+                        color: const Color.fromRGBO(66, 165, 245, 1),
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                          ),
+                          child: SingleChildScrollView(
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 5),
+                              height: 600,
+                              color: const Color.fromRGBO(66, 165, 245, 1),
+                              child: WebView(
+                                initialUrl:
+                                    'https://twitter.com/Tesla/status/1582901412312207361',
+                                javascriptMode: JavascriptMode.unrestricted,
+                                onWebViewCreated: (controller) {
+                                  _controller.complete(controller);
+                                },
+                                onProgress: (int progress) {
+                                  print(
+                                      'WebView is loading (progress : $progress%)');
+                                },
+                                navigationDelegate:
+                                    (NavigationRequest request) {
+                                  if (request.url
+                                      .startsWith('https://www.youtube.com/')) {
+                                    print('blocking navigation to $request}');
+                                    return NavigationDecision.prevent;
+                                  }
+                                  print('allowing navigation to $request');
+                                  return NavigationDecision.navigate;
+                                },
+                                onPageStarted: (String url) {
+                                  print('Page started loading: $url');
+                                },
+                                onPageFinished: (String url) {},
+                                gestureNavigationEnabled: true,
+                                backgroundColor: Colors.blue[400],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-              ]),
+              ),
             ),
           ],
         ),
